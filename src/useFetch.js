13f0f1adf535ab264ad87f0f17a980b12911da9e
sohail313 <shaikhsohail97692@gmail.com>
2021-03-26 15:52:30 +0530

    cleanup function and AbortControll

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,8 +6,10 @@ const useFetch = (url) =>{
     const [error, setError] =useState(null);
 
     useEffect(() => {
+        const abortCont = new AbortController();
+
         setTimeout(() => {
-            fetch(url)
+            fetch(url , { signal : abortCont.signal })
                 .then(res => {
                     if(!res.ok){
                         throw Error("Unable to fetch data from server");
@@ -20,10 +22,17 @@ const useFetch = (url) =>{
                     setError(null);
                 })
                 .catch(err =>{
-                    setIsPending(false);
-                    setError(err.message);
+                    if(err.name === 'AbortError'){
+                        console.log("Fetch aborted");
+                    }
+                    else{
+                        setIsPending(false);
+                        setError(err.message);
+                    } 
                 })
-        }, 1000);    
+        }, 1000);
+        return () => abortCont.abort();
+        
     },[url]);
     return {data,isPending,error};
 }
